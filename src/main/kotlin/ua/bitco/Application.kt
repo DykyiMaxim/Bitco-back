f4daf82e0a7f07e6ff0d6e66c29c4fc156112dfe
Dykyi Maksym <108894931+DykyiMaxim@users.noreply.github.com>
2022-11-11 17:41:04 -0400

    Working on connection to heroku PostgreSQL

--- src/main/kotlin/ua/bitco/Application.kt
+++ src/main/kotlin/ua/bitco/Application.kt
@@ -1,8 +1,13 @@
 package ua.bitco
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
+import io.ktor.server.locations.*
 import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import ua.bitco.data.repository.DataBaseFactory
 import ua.bitco.plugins.*
 
 fun main() {
@@ -10,7 +15,19 @@ fun main() {
         .start(wait = true)
 }
 
+@OptIn(KtorExperimentalLocationsAPI::class)
 fun Application.module() {
     configureSerialization()
     configureRouting()
+
+    DataBaseFactory.init()
+    install(Locations) {
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+}
 }
---
+++ src/main/kotlin/ua/bitco/data/repository/DataBaseFactory.kt
@@ -0,0 +1,52 @@
+package ua.bitco.data.repository
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import data.Tables.UserTable
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.net.URI
+
+object DataBaseFactory {
+
+    fun init(){
+        Database.connect(hikari())
+
+
+        transaction {
+            SchemaUtils.create(UserTable)
+        }
+    }
+
+
+    fun hikari():HikariDataSource{
+        val config = HikariConfig()
+
+        config.driverClassName = System.getenv("JDBC_DRIVER")
+       // config.jdbcUrl = System.getenv("DATABASE_URL")
+        config.maximumPoolSize = 3
+        config.isAutoCommit = false
+        config.transactionIsolation="TRANSACTION_REPEATABLE_READ"
+
+        val uri = URI(System.getenv("DATABASE_URL"))
+        val username = uri.userInfo.split(":").toTypedArray()[0]
+        val password = uri.userInfo.split(":").toTypedArray()[1]
+
+        config.jdbcUrl = "jdbc:postgresql://"+uri.host+":"+uri.path+"?sslmode=require"+"&user=$username&password=$password"
+
+
+        config.validate()
+
+        return HikariDataSource(config)
+    }
+
+    suspend fun <T> dbQuery(block:()->T):T =
+        withContext(Dispatchers.IO){
+            transaction { block() }
+        }
+
+
+}
